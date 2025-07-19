4ce913c5b91516bf0081387073504aea548941bf
pigeon-palace <139773809+pigeon-palace@users.noreply.github.com>
2025-07-19 11:21:50 -0500

    Add files via upload

--- table.js
+++ table.js
@@ -2,12 +2,41 @@ var dataSet = [];
 
 var table = new DataTable('#example', {
      columns: [
-        {"title": "Short", data: "short"},
-        {"title": "Name", data: "name"},
-        {"title": "Type", data: "type"},
+        {
+            "title": "Short", 
+            data: "short"
+        },
+        {
+            "title": "Name", 
+            data: "name",
+            render: function (data, type) {
+                if (type === 'display') {
+                    return '<a href="' + data["homepage"] + '">' + data["name"] + '</a>';
+                }
+ 
+                return data["name"];
+            }
+        },
+        {
+            "title": "Type", 
+            data: "type"
+        },
         {"title": "Country", data: "country"},
-        {"title": "Amount", data: "amount"},
-        {"title": "Source", data: "source"}
+        {
+            "title": "Amount", 
+            data: "amount",
+            render: function (data, type) {
+                if (type === 'display') {
+                    return '<a href="' + data[0]["source"] + '">' + data[0]["amount"].toLocaleString(
+                                              undefined, // leave undefined to use the visitor's browser 
+                                                         // locale or a string like 'en-US' to override it.
+                                              { minimumFractionDigits: 2 }
+                                            ); + '</a>';
+                }
+ 
+                return data[0]["amount"];
+            }
+        }
     ],
     data: dataSet,
     order: [4, "desc"]
