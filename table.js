de0944475bc889a8e2bb666efb86e27b11eca3ea
pigeon-palace <139773809+pigeon-palace@users.noreply.github.com>
2025-07-19 13:52:26 -0500

    Add files via upload

--- table.js
+++ table.js
@@ -36,18 +36,39 @@ var table = new DataTable('#example', {
  
                 return data[0]["amount"];
             }
+        },
+        {
+            "title": "Amount (USD)", 
+            data: "amount",
+            render: function (data, type) {
+                if (type === 'display') {
+                    return '<a href="' + data[0]["source"] + '">$' + (price * data[0]["amount"]).toLocaleString(
+                                              undefined, // leave undefined to use the visitor's browser 
+                                                         // locale or a string like 'en-US' to override it.
+                                              { minimumFractionDigits: 2 }
+                                            ); + '</a>';
+                }
+ 
+                return data[0]["amount"];
+            }
         }
     ],
     data: dataSet,
     order: [4, "desc"]
 })
 
+var price;
+
 async function fetchData(filename) { 
+    const priceResponse = await fetch("https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=litecoin")
+        .then(response => response.json()) // Parse JSON
+        .then(result => {price = result['litecoin']['usd'];}) // Parse JSON
+        .catch(error => console.error('Error fetching price JSON:', error));
     const response = await fetch("table.json")
         .then(response => response.json()) // Parse JSON
         .then(result => table.rows.add(result).draw()) // Parse JSON
         .catch(error => console.error('Error fetching JSON:', error));
-    return response;
+    return [response, priceResponse];
 }
 
 async function load() {
