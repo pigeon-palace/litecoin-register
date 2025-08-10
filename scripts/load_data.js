e4091ee554391fc3b5fd7d667a7a4972990a44e1
Aaron Crenshaw <acrenshaw@runbox.com>
2025-08-10 07:25:07 -0500

    add ability to import data from submodule

--- scripts/load_data.js
+++ scripts/load_data.js
@@ -33,4 +33,53 @@ async function load_data() {
         .then(response_json => {
             data = response_json;
         }) 
+        
+    await importData("MWEB");
+    await importData("cbLTC");
+    console.log(data);
+}
+
+async function importData(short) {
+    try {
+        const target = "../litecoin-register-scraper/data/" + short + ".csv"; 
+        
+        const res = await fetch(target, {
+            method: 'get',
+            headers: {
+                'content-type': 'text/csv;charset=UTF-8'
+            }
+        });
+
+        if (res.status === 200) {
+            const csv = parseCSV(await res.text());
+            for(var i = 0; i < data.length; i++){
+                if(data[i]["short"] == short){
+                    csv.forEach(item => data[i]['events'].push(item));
+                    data[i]['events'].sort(function(a, b){ return (new Date(b["date"])).getTime() - (new Date(a["date"])).getTime()});
+                    break;
+                }
+            }
+        } else {
+            console.log(`Error code ${res.status}`);
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+function parseCSV(csv){
+    csv = Papa.parse(csv).data;
+    var out = []
+    for(var i = 0; i < csv.length; i++){
+        if(csv[i].length < 4){
+            continue;
+        }
+        var out_add = {};
+        out_add['amount'] = parseFloat(csv[i][0]);
+        out_add['source'] = csv[i][1];
+        out_add['date'] = csv[i][2];
+        out_add['approx'] = csv[i][3] == "true";
+        out.push(out_add);
+    }
+    return out;
 }
